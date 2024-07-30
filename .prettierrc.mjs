deaa91ca2bec212dbf40e8a07cf472a852e75784
Moritz Heinrich <moritz.heinrich@cronn.de>
2024-07-30 16:28:35 +0200

    Chore: Fix prettier config

--- .prettierrc.mjs
+++ .prettierrc.mjs
@@ -1,10 +1,5 @@
 /** @type {import("prettier").Config} */
 export default {
-  trailingComma: "es5",
-  printWidth: 100,
-  tabWidth: 4,
-  semi: false,
-  singleQuote: true,
   plugins: ["prettier-plugin-astro", "prettier-plugin-tailwindcss"],
   overrides: [
     {
